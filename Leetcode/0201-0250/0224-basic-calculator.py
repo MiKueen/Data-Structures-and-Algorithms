71851a1de01af3138daab08c7488617ebcc3fc94
MiKueen <keshvi2298@gmail.com>
2019-10-08 18:27:39 +0530

    Added stack questions

---
+++ Leetcode/0201-0250/0224-basic-calculator.py
@@ -0,0 +1,59 @@
+'''
+Author : MiKueen
+Level : Hard
+Problem Statement : Basic Calculator
+
+Implement a basic calculator to evaluate a simple expression string.
+The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+Example 1:
+Input: "1 + 1"
+Output: 2
+
+Example 2:
+Input: " 2-1 + 2 "
+Output: 3
+
+Example 3:
+Input: "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+Note:
+You may assume that the given expression is always valid.
+Do not use the eval built-in library function.
+'''
+
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """ 
+        stack = []
+        operand = res = 0 
+        sign = 1  
+
+        for ch in s:
+            if ch.isdigit():
+                operand = (operand * 10) + int(ch)
+
+            elif ch == '+':
+                res += sign * operand
+                sign, operand = 1, 0
+
+            elif ch == '-':
+                res += sign * operand
+                sign, operand = -1, 0
+
+            elif ch == '(':
+                stack.append(res)
+                stack.append(sign)
+                sign, res = 1, 0
+
+            elif ch == ')':
+                res += sign * operand
+                res *= stack.pop()
+                res += stack.pop()
+                operand = 0
+
+        return res + sign * operand  
+              
