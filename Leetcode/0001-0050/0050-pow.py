157caa9b3307d549af7883915d3bd9d83739269a
MiKueen <keshvi2298@gmail.com>
2020-07-14 21:37:21 +0530

    Added solution 50

---
+++ Leetcode/0001-0050/0050-pow.py
@@ -0,0 +1,41 @@
+'''
+Author : MiKueen
+Level : Medium
+Problem Statement : Pow(x, n)
+
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+Input: 2.00000, 10
+Output: 1024.00000
+
+Example 2:
+Input: 2.10000, 3
+Output: 9.26100
+
+Example 3:
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+
+Note:
+* -100.0 < x < 100.0
+* n is a 32-bit signed integer, within the range [−231, 231 − 1]
+'''
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        res = 1
+        if n == 0: 
+            return 1
+        elif n < 0: 
+            x = 1/x
+            n = -n
+        prod, temp = x, n
+        while temp > 0:
+            if temp % 2 == 1: 
+                res *= prod
+            prod *= prod
+            temp //= 2
+        return res
+        
