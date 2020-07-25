ff39d4d6b4139c1137ccc8b15ec0100b0924e7b0
MiKueen <keshvi2298@gmail.com>
2020-07-25 19:53:40 +0530

    Added another approach to solution 41

--- Leetcode/0001-0050/0041-find-missing-positive.py
+++ Leetcode/0001-0050/0041-find-missing-positive.py
@@ -23,9 +23,10 @@ Your algorithm should run in O(n) time and uses constant extra space.
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        n = len(nums)
-        
+        '''
+        # Method 1
         # mark those elements who aren't in the range of [1, n]
+        n = len(nums)
         for i in range(n):
             if nums[i] <= 0 or nums[i] > n:
                 nums[i] = n + 1
@@ -41,5 +42,19 @@ class Solution:
                 return i + 1
         
         # if all elements are negative/marked means all elements are in the range of [1, n]
+        return n + 1
+        '''
+        
+        # Method 2
+        n = len(nums)
+        for i in range(n):
+            pos = nums[i] - 1
+            while 1 <= nums[i] <= n and nums[i] != nums[pos]:
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos = nums[i] - 1
+        
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
         return n + 1
         
