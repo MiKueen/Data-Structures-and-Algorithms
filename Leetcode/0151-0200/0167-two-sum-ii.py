ec621126e6be0c77dd772c3dba9bdd908d29cad0
MiKueen <keshvi2298@gmail.com>
2020-07-29 20:11:30 +0530

    Added solution 167

---
+++ Leetcode/0151-0200/0167-two-sum-ii.py
@@ -0,0 +1,30 @@
+'''
+Author : MiKueen
+Level : Easy
+Problem Statement : Two Sum II - Input array is sorted
+
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+Example:
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+'''
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while l < r:
+            res = numbers[l] + numbers[r]
+            if res == target:
+                return [l+1, r+1]
+            elif res < target:
+                l += 1
+            else:
+                r -= 1
+                
